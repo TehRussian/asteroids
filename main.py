7415cd903b2623f339837e19d35055fc08ad8107
TehRussian <alexanderdudin@yahoo.ca>
2025-03-02 16:49:42 -0700

    2: add player and player movement

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+        
+        player.update(dt)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
+        
+        
         
     
 if __name__ == "__main__":
